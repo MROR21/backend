a8169941904a15878cf8bdc2c6b4f902f9160498
Miguel Rafael Oliveira Rodrigues <miguel.rafael@agilean.com.br>
2026-04-02 13:58:31 -0300

    feat(domain): adicionando e configurando a entidade de domínio User

---
+++ Desafio-root-API/Desafio-root/Domain/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using Desafio_root.Domain.ValueObject;
+
+namespace Desafio_root.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public Email Email { get; private set; }
+
+        protected User() { }
+
+        private User(string name, Email email)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Email = email;
+        }
+
+        public static User Create(string name, Email email)
+        {
+            return new User(name, email);
+        }
+    }
+}

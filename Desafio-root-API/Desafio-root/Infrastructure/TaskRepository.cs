5916370f27fc6875ea141ceed2d1d0eb91d6adf0
Miguel Rafael Oliveira Rodrigues <miguel.rafael@agilean.com.br>
2026-04-04 18:54:36 -0300

    feat(infrastructure): implementar AppDbContext e repositórios de persistência

---
+++ Desafio-root-API/Desafio-root/Infrastructure/TaskRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Desafio_root.Domain.Entities;
+using Desafio_root.Domain.IRepository;
+
+namespace Desafio_root.Infrastructure
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly AppDbContext _context;
+
+        public TaskRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TaskItem?> GetByIdAsync(Guid id)
+        {
+            return await _context.Tasks.FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task<IEnumerable<TaskItem>> GetAllByUserIdAsync(Guid userId)
+        {
+            return await _context.Tasks.Where(t => t.UserId == userId).ToListAsync();
+        }
+
+        public async Task ToAddAsync(TaskItem task)
+        {
+            await _context.Tasks.AddAsync(task);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task ToUpdateAsync(TaskItem task)
+        {
+            _context.Tasks.Update(task);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task ToRemoveAsync(TaskItem task)
+        {
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Desafio-root-API/Desafio-root/Infrastructure/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Desafio_root.Domain.Entities;
+using Desafio_root.Domain.IRepository;
+
+namespace Desafio_root.Infrastructure
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            var emailVO = Desafio_root.Domain.ValueObject.Email.Create(email);
+
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == emailVO);
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}

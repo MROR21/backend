477ebb071108c4d1350b36178f582049d177dae3
Miguel Rafael Oliveira Rodrigues <miguel.rafael@agilean.com.br>
2026-04-04 19:39:53 -0300

    feat(infra): adicionar migrations iniciais e configuração de value objects

---
+++ Desafio-root-API/Desafio-root/Migrations/20260331020330_UpdateValueObjects.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Desafio_root.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateValueObjects : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

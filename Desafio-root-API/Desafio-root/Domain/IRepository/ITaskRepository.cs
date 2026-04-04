32502082e0626bfa80bd6af6a55eebc03961def1
Miguel Rafael Oliveira Rodrigues <miguel.rafael@agilean.com.br>
2026-04-04 11:06:59 -0300

    feat(domain): definir interfaces de contrato para repositórios

---
+++ Desafio-root-API/Desafio-root/Domain/IRepository/ITaskRepository.cs
@@ -0,0 +1,16 @@
+﻿using Desafio_root.Domain.Entities;
+namespace Desafio_root.Domain.IRepository
+{
+    public interface ITaskRepository
+    {
+        
+        Task <TaskItem?> GetByIdAsync (Guid id);
+        Task<IEnumerable<TaskItem>> GetAllByUserIdAsync(Guid userId);
+        Task ToAddAsync(TaskItem task);
+        Task ToUpdateAsync(TaskItem task);
+        Task ToRemoveAsync(TaskItem task);
+
+
+    }
+}
+
---
+++ Desafio-root-API/Desafio-root/Domain/IRepository/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Desafio_root.Domain.Entities;
+
+namespace Desafio_root.Domain.IRepository
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByEmailAsync(string email);
+
+        Task AddAsync(User user);
+    }
+}

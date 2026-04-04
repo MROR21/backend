83f522f6d11c93b2b590d97770715fbcb9962b4f
Miguel Rafael Oliveira Rodrigues <miguel.rafael@agilean.com.br>
2026-04-04 10:43:19 -0300

    feat(domain): implementar objetos de valor e validações de e-mail e prioridade

---
+++ Desafio-root-API/Desafio-root/Domain/ValueObject/Email.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Desafio_root.Domain.ValueObject
+{
+    public record Email
+    {
+        public string Value { get; init; }
+
+        private Email(string value) {
+
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("O e-mail não pode estar vazio.");
+
+            var regex = new Regex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+
+            if (!regex.IsMatch(value))
+                throw new ArgumentException("Formato de e-mail inválido. O e-mail deve conter '@' e um domínio válido.");
+
+            Value = value.ToLower().Trim();
+        }
+
+        public static Email Create(string value)
+        {
+            return new Email(value);
+        }
+    }
+}

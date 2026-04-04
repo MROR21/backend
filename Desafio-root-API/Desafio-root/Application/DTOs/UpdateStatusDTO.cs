4764147265b231e6d9e32b80517d99e1fdff7146
Miguel Rafael Oliveira Rodrigues <miguel.rafael@agilean.com.br>
2026-04-04 17:56:19 -0300

    feat(application): adicionar DTOs de entrada e saída para contratos da API

---
+++ Desafio-root-API/Desafio-root/Application/DTOs/UpdateStatusDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Desafio_root.Application.DTOs
+{
+    public record UpdateStatusDTO(string Status)
+    {
+
+    }
+}
---
+++ Desafio-root-API/Desafio-root/Application/DTOs/UserLoginDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace Desafio_root.Application.DTOs
+{
+    public record UserLoginDTO(string Name, string Email)
+    {
+    }
+}

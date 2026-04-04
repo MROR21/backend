466d96baa647219f54bb981dc1b6c72dee6ff29a
Miguel Rafael Oliveira Rodrigues <miguel.rafael@agilean.com.br>
2026-04-04 10:43:19 -0300

    feat(domain): implementar objetos de valor e validações de e-mail e prioridade

---
+++ Desafio-root-API/Desafio-root/Domain/ValueObject/TaskTitle.cs
@@ -0,0 +1,27 @@
+﻿using static System.Net.WebRequestMethods;
+
+namespace Desafio_root.Domain.ValueObject
+{
+    public record TaskTitle
+    {
+        public string Title { get; init; }
+
+        private TaskTitle(string title) 
+        {
+            Title = title; 
+        }
+
+
+        public static TaskTitle Create(string title)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+                throw new ArgumentException(" O Titulo não pode estar vazio.", nameof(title));
+
+            if (title.Length > 100)
+                throw new ArgumentException("Máximo de 100 caracteres", nameof(title));
+
+            return new TaskTitle(title);
+        }
+
+}
+}
